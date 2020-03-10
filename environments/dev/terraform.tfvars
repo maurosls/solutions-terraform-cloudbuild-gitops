843f0190b704924434e85878a78f48ed242ba49b
maurojr <mauro.santos@marlo.com.au>
2020-03-10 12:25:03 -0300

    Updating project ID vars

--- environments/dev/terraform.tfvars
+++ environments/dev/terraform.tfvars
@@ -1,1 +1,1 @@
-project="PROJECT_ID"
+project="marlo-digital-platform"
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "marlo-digital-platform-tfstate"
     prefix = "env/prod"
   }
 }
